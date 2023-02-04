f87224778953afd6bdcfab186976cd1ff700ddfd
Vlad <wladimir.pushkutse@gmx.de>
2023-02-04 11:43:31 +0100

    Helper added 2

---
+++ WebShop/app/Models/item.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\item
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|item newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|item newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|item query()
+ * @mixin \Eloquent
+ */
+class item extends Model
+{
+    use HasFactory;
+}
